d486a3e9c7f9d6608f62b47093648dd243334c88
Supun Thilakarathne <Thilakarathnesupun@gmail.com>
2020-09-09 23:30:04 +0530

    login page

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-offset-6 centered" >
+                <h1 > User Login</h1>
+            </div>
+        </div>
+
+    </div>
+    
+</body>
+</html>
